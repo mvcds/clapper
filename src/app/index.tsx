62d4ef91c6585440811211fa5e59701044718374
Marcos Vinícius C. da Silva <marvinc.silva@gmail.com>
2017-11-11 16:23:02 -0200

    add linter

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,13 @@
-import * as React from "react"
-import * as ReactDOM from "react-dom"
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
-interface HelloProps { compiler: string; framework: string; }
+interface Props { compiler: string; framework: string }
 
-class Hello extends React.Component<HelloProps, {}> {
-    render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
-    }
+function Hello(props: Props) {
+  return <h1>Hello from {props.compiler} and {props.framework}!</h1>
 }
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+  <Hello compiler="TypeScript" framework="React" />,
+  document.getElementById('example')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,26 @@
+const path = require('path')
+
 module.exports = {
-    entry: "./src/app/index.tsx",
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
-    },
-    devtool: "source-map",
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "awesome-typescript-loader"
-            },
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                loader: "source-map-loader"
-            }
-        ]
-    }
+  entry: './src/app/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      }
+    ]
+  }
 }
